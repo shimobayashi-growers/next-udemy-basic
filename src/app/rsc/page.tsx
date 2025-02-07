935b50a9b168a01a25878d8aba46ec345c4cb8d8
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-07 16:41:12 +0900

    chore: componentsにrccを作成して、rscで表示

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,8 +1,11 @@
+import ClientComponent from "@/components/ClientComponent";
+
 export default function ServerComponent() {
     console.log("Server Component");
     return (
         <div>
             <h1>Server Component</h1>
+            <ClientComponent />
         </div>
     )
 }
