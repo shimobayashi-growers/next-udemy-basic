5f046e86f42fb5cabf42c92431ec8a000491f4ff
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-07 16:36:45 +0900

    chore: use clientの確認

--- src/app/rcc/page.tsx
+++ src/app/rcc/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 
+import { useState } from "react";
+
 export default function ClientComponent() {
+    const [ count, setCount ] = useState(0);
+
     console.log("Client Component");
     return (
         <div>
             <h1>Client Component</h1>
+            <button onClick={() => setCount(count + 1)}>
+                Count: {count}
+            </button>
         </div>
     )
 }
