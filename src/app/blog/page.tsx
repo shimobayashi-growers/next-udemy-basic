e493cb6b9beaad3561ab735c45187094d3405747
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-07 16:05:07 +0900

    chore: errorページ作成

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -17,6 +17,7 @@ const articles = [
 // 3秒待機
 async function fetchArticles() {
     await new Promise(resolve => setTimeout(resolve, 3000));
+    // throw new Error("Error");
     return articles;
 }
 
