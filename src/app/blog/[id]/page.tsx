7586b3809201ffb2383f5859a6b73e9b19c72a3f
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-07 15:42:01 +0900

    chore: Promiseオブジェクトの型指定

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,5 +1,11 @@
-export default async function page({ params }: { params: { id: string } }) {
-  console.log(params);
+// 型を定義
+type Params = {
+    params: Promise<{
+        id: string
+    }>
+}
+
+export default async function page({ params }: Params ) {
   const {id} = await params;
   
     return (
