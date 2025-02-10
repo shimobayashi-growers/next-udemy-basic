876ecd7a3aaa0a5af74b739879675f9538eefbbc
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-10 22:03:02 +0900

    feat: contactフォームの追加

---
+++ src/app/contacts/complete/page.tsx
@@ -0,0 +1,7 @@
+export default function CompletePage() {
+    return (
+        <div>
+            <h1>Complete</h1>
+        </div>
+    )
+}
---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,9 @@
+import ContactForm from "@/components/ContactForm.tsx";
+
+export default function ContactsPage() {
+    return (
+        <div>
+            <ContactForm></ContactForm>
+        </div>
+    )
+}
