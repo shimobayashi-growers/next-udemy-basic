538310958430a7a17bb5f72d4568105a3449679a
Takaaki Shimobayashi <takaaki.shimobayashi@growers.co.jp>
2025-02-11 14:53:22 +0900

    chore: サーバサイドのバリデーション

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -3,7 +3,22 @@
 import { ContactSchema } from "@/validations/contact"
 import { redirect } from "next/navigation"
 
-export async function submitContactForm(formData: FormData) {
+// ActionStateの型定義
+type ActionState = {
+    success: boolean
+    errors: {
+        name?: string[]
+        email?: string[]
+    };
+    serverErrors?: string
+}
+
+
+export async function submitContactForm(
+    prevState: ActionState,
+    formData: FormData
+): Promise<ActionState> 
+{
     const name = formData.get('name')
     const email = formData.get('email')
 
@@ -13,9 +28,15 @@ export async function submitContactForm(formData: FormData) {
         email
     })
     if (!validationResult.success) {
-        const errors = validationResult.error.flatten()
+        const errors = validationResult.error.flatten().fieldErrors
         console.log(errors)
-        return {}
+        return {
+            success: false,
+            errors: {
+                name: errors.name || [],
+                email: errors.email || []
+            }
+        }
     }
     // DB登録
 
